455e2d757678ef7a2b60796aaf6ba1c68d2bee73
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-07 15:06:33 -0300

    Difereça de variável para constante

--- src/TypeVariables.java
+++ src/TypeVariables.java
@@ -2,5 +2,14 @@ public class TypeVariables {
     public static void main(String[] args) {
         double value = 2550D;
         System.out.println("About double: " + value);
+
+        int number = 2;
+        number = 10;
+
+        System.out.println(number);
+
+        final double VALUE_OF_PI = 3.14;
+
+        System.out.println("Constant values do not change: " + VALUE_OF_PI);
     }
 }
