f18cce4b25e0a751570c174c93f948267f4338ad
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-07 14:31:46 -0300

    Vendo sobre identação

---
+++ src/StudentBulletin.java
@@ -0,0 +1,10 @@
+public class StudentBulletin {
+    public static void main(String[] args) {
+        int averageFinal = 3;
+        if(averageFinal<6) {
+            System.out.println("DISAPPROVED");
+        } else {
+            System.out.println("APPROVED");
+        }
+    }
+}
